69ddcc7f5ae181ff119bc176c0fda91ca8fa7d69
EugeneLazakovich <zlazakovich@gmail.com>
2019-07-25 13:38:36 +0100

    Homework12, Lazakovich

---
+++ 12.1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _12._1
+{
+    class Gen<T> where T : Item
+    {
+        T ob;
+        public Gen(T ob)
+        {
+            this.ob = ob;
+        }
+        public void getUltimate()
+        {
+            ob.getUltimate();
+        }
+        public override string ToString()
+        {
+            return ob.ToString();
+        }
+    }
+    abstract class Item
+    {
+        protected String name;
+        protected int intel;
+        protected int agil;
+        protected int stren;
+        public Item(String name, int intel, int agil, int stren)
+        {
+            this.name = name;
+            this.intel = intel;
+            this.agil = agil;
+            this.stren = stren;
+        }
+        public abstract void getUltimate();
+        public override string ToString()
+        {
+            return "Name: " + name + "; Intelligence: " + intel + "; Agility: " + agil + "; Strength: " + stren;
+        }
+    }
+    class PhaseBoots : Item
+    {
+        public PhaseBoots(String name, int intel, int agil, int stren) : base(name, intel, agil, stren) { }
+        public override void getUltimate()
+        {
+            Console.WriteLine("PhaseBoots's Ultimate");
+        }
+    }
+    class MantaStyle : Item
+    {
+        public MantaStyle(String name, int intel, int agil, int stren) : base(name, intel, agil, stren) { }
+        public override void getUltimate()
+        {
+            Console.WriteLine("MantaStyle's Ultimate");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gen<Item> gen = new Gen<Item>(new PhaseBoots("PhaseBoots", 10, 25, 12));
+            Console.WriteLine(gen);
+            gen.getUltimate();
+            Console.WriteLine("---------------------");
+            List<Item> stringList = new List<Item>();
+            stringList.Add(new PhaseBoots("PhaseBoots", 10, 25, 12));
+            stringList.Add(new MantaStyle("MantaStyle", 23, 13, 13));
+            stringList.Add(new PhaseBoots("PhaseBoots", 11, 23, 14));
+            foreach(Item str in stringList)
+            {
+                Console.WriteLine(str);                
+            }
+        }
+    }
+}
---
+++ 12.2.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+
+
+namespace _12._2
+{
+    class MyCollection : ICollection, IList
+    {
+        int[] array = new int[4];
+        int count = 0;
+        public int Count()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public int Capacity()
+        {
+            return array.Length;
+        }
+
+        public bool IsSynchronized => throw new NotImplementedException();
+
+        public object SyncRoot => throw new NotImplementedException();
+
+        public bool IsFixedSize => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        int ICollection.Count => throw new NotImplementedException();
+
+        public object this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Add(object element)
+        {
+            Boolean flag = false;
+            int count = 0;
+            do
+            {
+                for (int i = count; i < array.Length; i++)
+                {
+                    if (array[count] == 0)
+                    {
+                        array[count] = (int)element;
+                        flag = true;
+                        break;
+                    }
+                    count++;
+                }
+                if (flag == false)
+                {
+                    array = new int[count * 2];
+                }
+            } while (flag);
+            return 1;
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(object element)
+        {
+            int[] newAr = new int[array.Length];
+            for(int i=0; i<array.Length; i++)
+            {
+                if(array[i] == (int)element)
+                {
+                    Array.Copy(array, 0, newAr, 0, i);
+                    Array.Copy(array, i + 1, newAr, i, array.Length - i - 1);
+                    newAr[array.Length - 1] = 0;
+                }
+            }
+            array = newAr;
+        }
+
+        public void RemoveAt(int index)
+        {
+            int[] newAr = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == index)
+                {
+                    Array.Copy(array, 0, newAr, 0, i);
+                    Array.Copy(array, i + 1, newAr, i, array.Length - i - 1);
+                    newAr[array.Length - 1] = 0;
+                }
+            }
+            array = newAr;
+        }
+        public void GetInfo()
+        {
+            for(int i=0; i<array.Length; i++)
+            {
+                Console.WriteLine("array[" + i + "]: " + array[i]);
+            }
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
